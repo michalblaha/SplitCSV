69f7b9db29a717a57fc031c36d8a037103ad9a18
Michal Blaha <michal@michalblaha.cz>
2019-10-24 21:37:11 +0200

    SplitCSV v.1.0
    Split CSV file into smaller files.

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# Split CSV
+
+Split large CSV into smaller, valid CSV. CSV files must use UTF-8 encoding.
+
+`SplitCSV filename maxChunkSizeInBytes`
+
+Example: `SplitCSV data.csv 100000`
+- Splits data.csv into files with name `data_0001.csv`, `data_0002.csv`, `data_0003.csv` etc. Any file won't be larger than 100000 bytes. 
+
+
+
+Header of CSV file (if is present) is considered as any other record. That's why present header is only in the first chunk.
+
---
+++ SplitCSV/Program.cs
@@ -0,0 +1,97 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Help(); return;
+            }
+
+            var ConsoleFColor = Console.ForegroundColor;
+
+            string fn = System.IO.Path.GetFullPath(args[0]);
+            string fnExt = System.IO.Path.GetExtension(fn);
+            string fnNoExt = fn.Substring(0,fn.Length-fnExt.Length);
+
+            if (!System.IO.File.Exists(fn))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"File {fn} not found.");
+                Console.ForegroundColor = ConsoleFColor;
+                return;
+            }
+            long chunkSize = 0;
+            if (!long.TryParse(args[1], out chunkSize))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Chunk size {args[1]} is not recongnized as number.");
+                Console.ForegroundColor = ConsoleFColor;
+                return;
+            }
+
+            long currSize = 0;
+            long actChunk = 1;
+
+            using (var reader = new StreamReader(fn))
+            {
+                using (var csv = new CsvReader(reader, new CsvHelper.Configuration.Configuration() { HasHeaderRecord = false, Delimiter = "," }))
+                {
+                    while (csv.Read())
+                    {
+
+                        var rec = csv.Context.Record;
+                        var recRaw = rec.Select(m =>
+                                    {
+                                        if (m.Contains((char)13) || m.Contains((char)10) || m.Contains("\t"))
+                                            return "\"" + m + "\"";
+                                        else
+                                            return m;
+                                    }
+                        ).Aggregate((f, s) => f + "," + s)
+                            + "\n";
+                        var recSize = Encoding.UTF8.GetByteCount(recRaw);
+                        if (currSize+recSize > chunkSize)
+                        {
+                            actChunk++;
+                            currSize = 0;
+                        }
+
+                        if (currSize == 0)
+                        {
+                            System.IO.File.Delete(chunkFn(fnNoExt,fnExt,actChunk));
+                            Console.WriteLine("Writing into " + chunkFn(fnNoExt, fnExt, actChunk));
+                        }
+
+                        currSize = currSize + recSize;
+
+                        System.IO.File.AppendAllText(chunkFn(fnNoExt, fnExt, actChunk), recRaw);
+                    }
+                }
+            }
+        }
+
+        static string chunkFn(string fnNoExt, string fnExt, long part)
+        {
+            return fnNoExt + "_" + part.ToString("0000") + fnExt;
+        }
+
+        static void Help()
+        {
+            Console.WriteLine(@"SplitCSV - split CSV to chunks
+SplitCSV filename maxChunkSizeInBytes
+
+
+");
+        }
+    }
+}
